cea58c1345e6c4000764e64f55023f0db409e3a2
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-07-04 19:31:25 +0200

    Update World3Farm.lua

--- Things/World3Farm.lua
+++ Things/World3Farm.lua
@@ -1,4 +1,4 @@
--- Сервисы Robloxвфвф31231313
+-- Сервисы Robloxвфвф31231313eqweqe
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
 
@@ -144,7 +144,7 @@ task.spawn(function()
                         hrp.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
                         
                         -- Ждем 5 секунд на предпоследней точке перед финальным рывком
-                        task.wait(10) 
+                        task.wait(20) 
                     end
 
                     -- Ещё раз чекаем условия после 5-секундного ожидания
